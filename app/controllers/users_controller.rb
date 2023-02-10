4252ff976f6cf75d115082285a7fa88521fd4909
Austin <baik.austin@gmail.com>
2023-02-10 15:50:25 -0800

    commit tree 779020d7c9bdca1172b44fdb6fbad9e2bc6ae1e2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id #still don't quite understand this
+    #   Sessions work behind the scenes because of the bycrypt gem. We don't need to pass session within the render json, because session will be an object that connects with the application when you install the gem
+
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
